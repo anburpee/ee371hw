f74daac38ec039bcf570446940fb308178de84d4
anburpee <ayaburpee@gmail.com>
2025-10-01 17:09:05 -0700

    removed commented out portion of p1

--- ee371hw1/src/hw1p1.sv
+++ ee371hw1/src/hw1p1.sv
@@ -5,37 +5,6 @@ module hw1p1 (clk, reset, x, y, s);
 	input logic clk, reset, x, y;
 	output logic s;
 
-// 	// states
-// 	enum {s0, s1} ps, ns; // ps = Q, ns = C
-
-// 	// next state logic
-// 	always_comb begin
-// 		case(ps)
-
-// 			s0:		ns = x & y ? s1 : s0;
-
-// 			s1: 	ns = ~(x & y) ? s0 : s1;
-
-// 		endcase
-// 	end // always_comb
-
-
-// 	// state update logic
-	// always_ff @(posedge clk) begin
-// 		if (reset)
-// 			ps <= s0;
-// 		else
-// 			ps <= ns;
-// 	end // always_ff
-
-
-//	// output logic
-// 	assign s = (((ps == s0) & (x ^ y)) | ((ps == s1) & ~(x ^ y)))
-
-
-// endmodule  // hw1p1
-
-
 // implementation with full adder
 	logic q, c;
 
